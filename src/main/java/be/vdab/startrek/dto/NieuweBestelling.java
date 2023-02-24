2efc9fc9c64d0bf7c014e79dd734245c2c7f696f
Peter D'hondt <dhondt.peter@gmail.com>
2023-02-24 21:39:09 +0100

    added tests for creating bestellingen

--- src/main/java/be/vdab/startrek/dto/NieuweBestelling.java
+++ src/main/java/be/vdab/startrek/dto/NieuweBestelling.java
@@ -1,9 +1,10 @@
 package be.vdab.startrek.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 import java.math.BigDecimal;
 
-public record NieuweBestelling(@NotBlank String omschrijving, @Positive BigDecimal bedrag) {
+public record NieuweBestelling(@NotBlank String omschrijving, @NotNull @Positive BigDecimal bedrag) {
 }
--- src/test/java/be/vdab/startrek/controllers/WerknemerControllerTest.java
+++ src/test/java/be/vdab/startrek/controllers/WerknemerControllerTest.java
@@ -1,14 +1,22 @@
 package be.vdab.startrek.controllers;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @Sql({ "/werknemers.sql", "/bestellingen.sql" })
@@ -16,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class WerknemerControllerTest extends AbstractTransactionalJUnit4SpringContextTests {
     private final static String WERKNEMERS = "werknemers";
     private final static String BESTELLINGEN = "bestellingen";
+    private final static Path TEST_RESOURCES = Path.of("src/test/resources");
     private final MockMvc mockMvc;
 
     WerknemerControllerTest(MockMvc mockMvc) {
@@ -40,5 +49,16 @@ class WerknemerControllerTest extends AbstractTransactionalJUnit4SpringContextTe
                         status().isOk(),
                         jsonPath("length()").value(
                                 countRowsInTableWhere(BESTELLINGEN, "werknemerId = " + werknemerId)));
+    }
+    @ParameterizedTest
+    @ValueSource(strings = { "bestellingMetLegeOmschrijving.json", "bestellingZonderOmschrijving.json",
+    "bestellingZonderBedrag.json", "bestellingMetNegatiefBedrag.json" })
+    void createMetVerkeerdeDataMislukt(String fileName) throws Exception {
+        var werknemerId = findIdTestWerknemer1();
+        var jsonData = Files.readString(TEST_RESOURCES.resolve(fileName));
+        mockMvc.perform(post("/werknemers/{werknemerId}/nieuwebestelling", werknemerId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonData))
+                .andExpect(status().isBadRequest());
     }
 }
